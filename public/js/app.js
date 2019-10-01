f6756905ec59ec9a24dabddf190ce7c28e7d7236
unknown <engr.waleedakhlaq@gmail.com>
2019-10-01 17:18:32 +0500

    Insert User Data to Database Using Axios in Laravel 5.7 and Vue

--- public/js/app.js
+++ public/js/app.js
@@ -38935,7 +38935,7 @@ var staticRenderFns = [
       _vm._v(" "),
       _c(
         "button",
-        { staticClass: "btn btn-primary", attrs: { type: "button" } },
+        { staticClass: "btn btn-primary", attrs: { type: "submit" } },
         [_vm._v("Save")]
       )
     ])
